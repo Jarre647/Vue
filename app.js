d1adbba91900c4c12f317e4f8ea366c6c715ae5f
Alexandr <39827518+Jarre647@users.noreply.github.com>
2019-11-07 22:21:53 +0700

    markup

--- app.js
+++ app.js
@@ -6,45 +6,22 @@ const cars = [
     car('Ford', 'Vyn', 'Eva', 2016, '+7 001 00-00', 'images/b.jpg'),
     car('MB', 'ron', 'Vasja', 2016, '+7 020 00-00', 'images/c.jpg'),
 ]
-
+const subMenus =[ ]
+Vue.component('submenu'
+{
+    data: subMenus,
+        template:''
+})
 
 new Vue({
     el: '#app',
     data: {
-        cars: cars,
-        car: cars[0],
-        selectedCarIndex: 0,
-        phoneVisibility: false,
-        search: '',
-        modalVisibility: false,
-        logs: []
+      
     },
     methods: {
-        selectCar(index) {
-            this.car = cars[index],
-                this.selectedCarIndex = index
-        },
-        newOrder() {
-            this.modalVisibility = false;
-            this.logs.push(
-                log(`Success order: ${this.car.name} - ${this.car.model}`, 'ok')
-            )
-        },
-        cancelOrder() {
-            this.modalVisibility = false;
-            this.logs.push(
-                log(`Success order: ${this.car.name} - ${this.car.model}`, 'cnl')
-            )
-        }
+     
     },
     computed: {
-        phoneBtnText() {
-            return this.phoneVisibility ? 'Hide phone' : 'Show phone'
-        },
-        filteredCars() {
-            return this.cars.filter(car => {
-                return car.name.indexOf(this.search) > -1 || car.model.indexOf(this.search) > -1
-            })
-        }
+      
     }
 });
