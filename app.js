268b96d1729d065b90dc029e1e60037cb6930d5d
Alexandr <39827518+Jarre647@users.noreply.github.com>
2019-11-07 19:23:50 +0700

    Revert "ReworkToSPA"
    This reverts commit 7787fa289427af0b4be97e7053778fed3e4428a3.

---
+++ app.js
@@ -0,0 +1,50 @@
+﻿const car = (name, model, owner, year, phone, image) => ({ name, model, owner, year, phone, image })
+const log = (text, type, date = new Date()) => (text, type, date)
+
+const cars = [
+    car('Bugatty', 'Veyron', 'Seva', 2016, '+7 000 00-00', 'images/a.jpg'),
+    car('Ford', 'Vyn', 'Eva', 2016, '+7 001 00-00', 'images/b.jpg'),
+    car('MB', 'ron', 'Vasja', 2016, '+7 020 00-00', 'images/c.jpg'),
+]
+
+
+new Vue({
+    el: '#app',
+    data: {
+        cars: cars,
+        car: cars[0],
+        selectedCarIndex: 0,
+        phoneVisibility: false,
+        search: '',
+        modalVisibility: false,
+        logs: []
+    },
+    methods: {
+        selectCar(index) {
+            this.car = cars[index],
+                this.selectedCarIndex = index
+        },
+        newOrder() {
+            this.modalVisibility = false;
+            this.logs.push(
+                log(`Success order: ${this.car.name} - ${this.car.model}`, 'ok')
+            )
+        },
+        cancelOrder() {
+            this.modalVisibility = false;
+            this.logs.push(
+                log(`Success order: ${this.car.name} - ${this.car.model}`, 'cnl')
+            )
+        }
+    },
+    computed: {
+        phoneBtnText() {
+            return this.phoneVisibility ? 'Hide phone' : 'Show phone'
+        },
+        filteredCars() {
+            return this.cars.filter(car => {
+                return car.name.indexOf(this.search) > -1 || car.model.indexOf(this.search) > -1
+            })
+        }
+    }
+});
