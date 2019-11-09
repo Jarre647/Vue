9e78b305603e688c99af66cf949fb8519b873780
Alexandr <39827518+Jarre647@users.noreply.github.com>
2019-11-09 16:00:21 +0700

    add menu

--- app.js
+++ app.js
@@ -13,12 +13,6 @@ const DescriptionsMotors = [
     descriptionMotor('ABLE ML631-2', 'single phase', '0,18', '220', '1.48', '2800', 'images/ml631.jpg')
 ];
 
-//Vue.component('submenu'
-//{
-//    data: subMenus,
-//        template:''
-//})
-
 let vue = new Vue({
     el: '#app',
     data: {
